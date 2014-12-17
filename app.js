93037cf53aa2e846fc3689a859a4c87d74b4ac16
Eli Xian <elixian@gmail.com>
2014-12-16 22:22:24 -0600

    use mongodb as explicit cache for player ready state, implement find and update functions to set state, clientside undergoing reconstruction

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var express = require('express'),
 		//fs = require('fs'),
 		//request = require('request'),
 		mongo = require('./db/mongo')
-		format = require('util').format,
+		//format = require('util').format,
+		roundRobin = require('./lib/util'),
 		tables = require('./routes/tables');
 
 var uri = 'mongodb://onepiece:luffylaw@ds063240.mongolab.com:63240/waieez';
 //Cache on server for the decks
 var coll;
+var gameRoom;
 var baseA = [];
 var baseQ = [];
 
@@ -20,26 +22,41 @@ var baseQ = [];
 io.on('connection', function (client){
 
 	console.log('client connected...'+client);
-
+	//need to change join to work only if not in middle of round.
+	//may need a listener/emitter that inserts new players that join in the middle of the round
 	client.on('join', function (data){
 		var username;
 		client.username = username = data.username;
 		var path = data.path;
-		var thisRoom = io.sockets.adapter.rooms[path];//rooms autoclose when length 0
 
+		var thisRoom = io.sockets.adapter.rooms[path];//rooms autoclose when length 0
 		if (!thisRoom) {
-			client.emit('welcome', 'You are the first one here')
-		};
+			client.emit('Server Message', 'You are the Czar! Start the game when ready');
+			upsertReadyPlayers(path, []);//path, player
+		}
 
 		//Join Handshake Sequence
 		client.join(path);
-		io.to(path).emit('welcome', client.username + ' has joined '+path);
-		console.log(client.username+' registered on server');
+
+		var msg = client.username + ' has joined '+path;
+		io.to(path).emit('Server Message', msg);
 		client.emit('pass deck', baseA);
-		console.log('client username set: '+ client.username);	
+		console.log('client username set: '+ client.username);
 	});
 
-
+	client.on('czar action', function (data){
+		io.to(data.path).emit('start', 'Game started!');
+	});
+	
+	client.on('player ready', function (data){
+		//var checkAllReady = function(path, player, numPlayers, successCall, failCall, playersReady){
+		//bind all the necessary values to checkall ready,
+		var numPlayers = Object.keys(io.sockets.adapter.rooms[data.path]).length;
+		var checkReady = checkAllReady.bind(null, data.path, client.id, numPlayers);
+		findAndUpdate(data.path, checkReady);
+		io.to(data.path).emit('waiting', data.players);	//needed anymore?
+		//io.to(data.path).emit('Server Message', numReady + '/' + numPlayers + ' players are ready');
+	});
 	//Start Round
 		//emit accepting answers --> triggers clickable clientside
 		//listen for responses (duration 10-30s) || num response = numplayers ?on new player join, refresh timer?
@@ -68,7 +85,6 @@ app.use('/', tables);
 
 //Start up server
 mongo.connect(uri, function (){
-
 	//init cache? server only needs to serve one deck for now
 	coll = mongo.collection('card');
 	coll.find({expansion:"Base", cardType: "A"},{_id: 0, text:1}).each(function (err, doc){
@@ -85,3 +101,33 @@ mongo.connect(uri, function (){
 		console.log('listen @ 3000..');	
 	});
 });
+
+
+var findAndUpdate = function(path, callback){
+	var gameRoom = mongo.collection('gameRoom');
+	gameRoom.findOne( {'path': path}, {'_id': 0, 'ready': 1}, function (err, result){
+		if (err) console.warn(err.message);
+		callback(result.ready)
+	});
+};
+
+var upsertReadyPlayers = function(path, player){
+	var ready = {ready: player};
+	var gameRoom = mongo.collection('gameRoom');
+	gameRoom.update( {'path': path}, {$set: ready}, {w:1, upsert:true}, function(err) {
+  	if (err) console.warn(err.message);
+  	else console.log('successfully updated');
+  });
+};
+
+// only way that makes sense to me, I'll admit its rather bad.
+var checkAllReady = function(path, player, numPlayers, playersReady){
+	if (numPlayers == playersReady.length){
+		//successCall();//emit ready
+	} else {
+		playersReady.push(player);
+		console.log(playersReady);
+		upsertReadyPlayers(path, playersReady);
+		//failCall();//emit waiting
+	}
+};
